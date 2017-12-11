094525bd4fa2eb35ebc6acb251fda365504182f3
Jummit <jummit@web.de>
2017-12-11 15:15:53 +0100

    removed color arg from layout

--- card_manager.lua
+++ card_manager.lua
@@ -2,7 +2,22 @@ card_engine = require "/card_engine"
 return {
   tree = card_engine.new_card(
     {
-      color={r=100,g=10,b=10}
+      color={r=100,g=10,b=10},
+      {
+        type="text",
+        text="Colorfull Circles",
+        x="middle",
+        y=13,
+        r=255,
+        g=255,
+        b=255
+      },
+      {
+        type="image",
+        image=love.graphics.newImage("/Assets/cardpictures/rings.png"),
+        x=8,
+        y=25
+      }
     }
   )
 }
